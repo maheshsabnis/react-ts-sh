b7271f958c13a001e249ec216ea4951e2856c512
Mahesh <sabnis_m@hotmail.com>
2025-08-12 03:48:26 +0000

    v

--- src/components/routingcomponentsmsal/authConfig.ts
+++ src/components/routingcomponentsmsal/authConfig.ts
@@ -1,6 +1,6 @@
 export const msalConfig = {
   auth: {
-    clientId: "3a7157fd-15e2-4f6a-acac-c09cee3fb89e",
+    clientId: "",
     authority: "https://login.microsoftonline.com/57084445-e440-441f-bc3f-fabe8cbb2535",
     redirectUri: "http://localhost:3000",
   },
